fdec699ead002f4b5a31030fa4ed6ac5b162aec6
GBTC405007UR <GBTC405007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-23 16:26:36 -0400

    interesting error

---
+++ src/main/java/com/example/demo/SpringBoot503exApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot503exApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot503exApplication.class, args);
+    }
+
+}
